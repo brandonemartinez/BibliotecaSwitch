20c7947108acd00f9ef3149f4a35367e0fe6b597
Brandon Martinez <brandmartinez96@gmail.com>
2021-05-09 19:55:47 -0300

    Creacion de un controller api para verificar el funcionamiento.

---
+++ Libro.BL/Controllers/TestApiController.cs
@@ -0,0 +1,35 @@
+﻿using LibroBL.Models;
+using LibroBL.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibroBL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestApiController : ControllerBase
+    {
+        AutorRepositorio foo = new AutorRepositorio();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var autores = foo.GetAutores();
+
+            if(autores !=null)
+            {
+                return Ok(autores);
+            }
+            else
+            {
+                return NotFound("No hay elementos en la lista");
+            }
+            
+            
+        }
+    }
+}
