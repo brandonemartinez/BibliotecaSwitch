6c903d87beda892e7dd55738248aede9eddc8e49
Brandon Martinez <brandmartinez96@gmail.com>
2021-05-11 16:23:41 -0300

    Se configura parte del login

---
+++ Libro.BL/Repositorio/LoginRepositorio.cs
@@ -0,0 +1,35 @@
+﻿using LibroBL.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibroBL.Repositorio
+{
+    public class LoginRepositorio
+    {
+
+        public Usuario Usuario(string usuario, string password)
+        {
+            Usuario helper = new Usuario();
+
+            using(BibliotecaContext context = new BibliotecaContext())
+            {
+                helper = context.Usuarios.Where(w => w.NombreUsuario == usuario && w.Pass == password).FirstOrDefault();
+            }
+
+            if (helper != null)
+            {
+                return helper;
+            }
+            else
+            {
+                return null;
+            }
+
+            
+        }
+
+    }
+}
